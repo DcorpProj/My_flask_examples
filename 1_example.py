33d3c9503c32339dd3f620eb22392494e96e3058
DcorpProj <dimabobkov2016@gmail.com>
2024-08-20 00:09:53 +0300

    1 Example (buttons)

---
+++ 1_example.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    return '''<title>Hello button (Flask)</title>
+    <p1>Hi button</p1>
+    <br>
+    <button type=submit>Hi!</button>
+    <button>A button.</button>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
